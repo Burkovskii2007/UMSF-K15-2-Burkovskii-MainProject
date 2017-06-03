32542fac5b0ac936a058147643270c6bc634c743
Burkovskii2007 <enderpootis@gmail.com>
2017-06-03 13:00:40 +0300

    12312
    123123

---
+++ zad1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сумму вклада =");
+            string x = Console.ReadLine();
+            double sum = double.Parse(x);
+            Console.WriteLine("Введите процент =");
+            string y = Console.ReadLine();
+            double perc = double.Parse(y);
+            double sum2 = (perc * sum) / 100 + sum;
+            Console.WriteLine("В конце года сумма будет равна " + sum2);
+            Console.ReadKey();
+
+
+            
+            
+
+        }
+    }
+}
